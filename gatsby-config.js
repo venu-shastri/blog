46547b863b0324635511d34ef2301a9933788b6f
venugopal <venu.shastri@gmail.com>
2022-04-14 23:05:14 +0530

    Add all my files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Venugopal Shastri Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Venugopal Shastri`,
+      summary: `who lives and works in Bangalore building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
